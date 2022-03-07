d4379f996f9d9aa9cc1a7de8c3598aab0aa2d097
Abhay Raj Singh <abhayraja4@gmail.com>
2022-03-07 00:14:21 -0500

    Add heroku in hosts

--- grocsite/settings.py
+++ grocsite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-n12v=86nl8+w@)c-ck+cbh0-8jpbbxue*dg*csek*)ap6%lzfk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['alphasingh.pythonanywhere.com', 'localhost']
+ALLOWED_HOSTS = ['alphasingh.pythonanywhere.com', 'localhost', 'g4django.herokuapp.com']
 
 # Application definition
 
