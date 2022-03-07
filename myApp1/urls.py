6e953880fbbd11f5831029c896d10c4aee21f9e8
Abhay Raj Singh <abhayraja4@gmail.com>
2022-03-07 14:45:48 -0500

    Static files and login admin

--- myApp1/urls.py
+++ myApp1/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'myApp1'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('detail/<int:type_no>', views.detail, name='detail'),
-    path('movies/', views.movies, name='movies'),
-    path('movies/<str:movie_id>', views.movie_detail, name='movie_detail'),
-    path('movies/<str:movie_id>/similar', views.similar_movies, name='similar_movies'),
-]
+                  path('', views.index, name='index'),
+                  path('about/', views.about, name='about'),
+                  path('detail/<int:type_no>', views.detail, name='detail'),
+                  path('movies/', views.movies, name='movies'),
+                  path('movies/<str:movie_id>', views.movie_detail, name='movie_detail'),
+                  path('movies/<str:movie_id>/similar', views.similar_movies, name='similar_movies'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
